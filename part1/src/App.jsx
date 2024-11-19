838500d25643891518a0bf69ee6226852a7fdfc9
Justin Gordon <justingordon@Justins-MacBook-Pro.local>
2024-11-19 15:50:43 -0700

    1.7 add a stats component

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,8 +1,29 @@
 import { useState } from 'react'
 
 const Button = ({handleClick, text}) => {
+  
   return (
     <button onClick={() => handleClick((prevState) => prevState + 1)}>{text}</button>
+  )
+}
+
+const Statistics = ({feedback}) => {
+  const total = feedback[0] + feedback[1] + feedback[2]
+  const average = (feedback[0] - feedback[2]) / total
+  const posPercent = (100 * feedback[0]) / total;
+
+
+
+  return (
+    <>
+      <h2>Stats</h2>
+      <p>Good: {feedback[0]}</p>
+      <p>Neutral: {feedback[1]}</p>
+      <p>Bad: {feedback[2]}</p>
+      <p>Total: {total}</p>
+      <p>Average: {average}</p>
+      <p>Percentage of Positive: {posPercent}</p>
+    </>
   )
 }
 
@@ -13,6 +34,7 @@ const App = () => {
   const [bad, setBad] = useState(0)
 
 
+  
 
   return (
     <>
@@ -22,10 +44,8 @@ const App = () => {
       <Button handleClick={setNeutral} text="neutral"/>
       <Button handleClick={setBad} text="bad"/>
     </div>
-    <h2>Stats</h2>
-    <p>Good: {good}</p>
-    <p>Neutral: {neutral}</p>
-    <p>Bad: {bad}</p>
+    <Statistics feedback={[good, neutral, bad]}/>
+
     </>
   )
 }
